aad93cfea7bbb2fe2337066f74ce0f4088f260b5
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2024-01-28 20:57:08 +0900

    Update prototype. Using softwareupdate command

--- install.sh
+++ install.sh
@@ -1,32 +1,25 @@
 #!/bin/zsh
 
 echo "Find Xcode Command Line Tools..."
-XCLT_PATH=`xcode-select -p`
+XCLT_PATH=`xcode-select -p 2> /dev/null`
 if [ $? -eq 0 ]; then
-  echo "Found Xcode Command Line Tools on ${XCLT_PATH}"
-  exit 0
+    echo "Found Xcode Command Line Tools on ${XCLT_PATH}"
+    exit
 fi
 
-echo "Install Xcode Command Line Tools by 'xcode-select --install'"
-xcode-select --install
-osascript > /dev/null << APPLESCRIPT
--- Waiting appear "Install Command Line Developer Tools" dialog
-delay 5
+echo "Install Xcode Command Line Tools by softwareupdate"
+SU_XCLT_FLUG_PATH=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+if [ ! -f ${SU_XCLT_FLUG_PATH} ]; then
+    touch ${SU_XCLT_FLUG_PATH}
+    exit
+fi
 
-tell application "Install Command Line Developer Tools"
-	activate
-	
-	tell application "System Events" to tell window 1 of application process "Install Command Line Developer Tools"
-		activate
-		
-		-- 1:Install button, 2:Cancel button
-		click button 1
-		
-		delay 1.5
-		
-		-- On "License Agreement" modal window
-		-- 1:Agree button, 2:UnAgree button
-		click button 1
-	end tell
-end tell
-APPLESCRIPT
+XCLT_NAME=`softwareupdate -l | grep "Label: Command Line Tools" | awk -F': ' '{print $2}'`
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+softwareupdate -i ${XCLT_NAME}
+if [ -f ${SU_XCLT_FLUG_PATH} ]; then
+    rm ${SU_XCLT_FLUG_PATH}
+fi
