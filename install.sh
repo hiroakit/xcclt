28cea45494349e634329743569ee6d31a3a4d882
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2024-01-28 16:41:22 +0900

    Add prototype

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+
+set -e
+
+echo "Find Xcode Command Line Tools..."
+XCLT_PATH=`xcode-select -p`
+if [ $? -eq 0 ]; then
+  echo "Found Xcode Command Line Tools on ${XCLT_PATH}"
+  exit 0
+fi
+
+echo "Install Xcode Command Line Tools by 'xcode-select install'"
+xcode-select install
+osascript << APPLESCRIPT |
+tell application "Install Command Line Developer Tools"
+	activate
+	
+	tell application "System Events" to tell window 1 of application process "Install Command Line Developer Tools"
+		activate
+		
+		-- 1:Install button, 2:Cancel button
+		click button 1
+		
+		delay 1.5
+		
+		-- On "License Agreement" modal window
+		-- 1:Agree button, 2:UnAgree button
+		click button 2
+	end tell
+end tell
+APPLESCRIPT
